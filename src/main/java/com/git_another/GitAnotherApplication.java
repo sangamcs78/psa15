967e103a336c08b9e5b0837e7f65a9cb9d3687fa
sangamcs78 <sangam0365207@gmail.com>
2024-10-25 16:33:53 +0530

    Defect 3457-Fixed the links not working in home page

---
+++ src/main/java/com/git_another/GitAnotherApplication.java
@@ -0,0 +1,13 @@
+package com.git_another;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAnotherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAnotherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git_another/GitAnotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_another;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAnotherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
